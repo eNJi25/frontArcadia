53ee277fc748848f1c194f9e6083c094a73551b0
eNJi25 <nfischer.contact@gmail.com>
2025-01-28 08:57:29 +0100

    feat : ajout de la page services.html + services.js + ajout de la route dans allRoutes + lien dans la navbar

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 // Toutes les routes à définir ici (url, titre de la page, chemin page HTML, chemin fichier JS)
 export const allRoutes = [
-    new Route("/", "Accueil", "pages/home.html", "assets/js/pages/home.js")
+  new Route("/", "Accueil", "pages/home.html", "assets/js/pages/home.js"),
+  new Route(
+    "/services",
+    "Nos Services",
+    "pages/services.html",
+    "assets/js/pages/services.js"
+  ),
 ];
 
 export const websiteName = "Arcadia";
